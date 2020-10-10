f96b175fe0b9bf2e55413129e5cf376c623cdb7f
Kevin Mulia <kevin.mulia@btpn.com>
2020-10-11 04:55:18 +0700

    add app service getData second unit test

--- src/app/service/app.service.spec.ts
+++ src/app/service/app.service.spec.ts
@@ -1,6 +1,7 @@
 import {TestBed} from '@angular/core/testing';
 import {AppService} from './app.service';
 import {HttpClient} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
 
 describe('AppService', () => {
   const mockHttpClient = {
@@ -27,6 +28,14 @@ describe('AppService', () => {
       appService.getData();
 
       expect(mockHttpClient.get).toHaveBeenCalledWith(expectedUrl);
+    });
+
+    it('should return observable data', () => {
+      mockHttpClient.get.mockReturnValueOnce(of({}));
+
+      const expectedData = appService.getData();
+
+      expect(expectedData).toBeInstanceOf(Observable);
     });
   });
 
