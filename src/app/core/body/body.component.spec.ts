1bcca9b82cc61e038d3c7168779c3a035937ed17
Kevin Mulia <kevin.mulia@btpn.com>
2020-10-12 00:57:52 +0700

    add styling for core components, add body component implementation

--- src/app/core/body/body.component.spec.ts
+++ src/app/core/body/body.component.spec.ts
@@ -1,10 +1,30 @@
 import {async, TestBed} from '@angular/core/testing';
 
 import {BodyComponent} from './body.component';
+import {CatFact} from '../../model/cat_fact.model';
+import {By} from '@angular/platform-browser';
 
 describe('BodyComponent', () => {
   let component;
   let fixture;
+  let debugElement;
+
+  const mockCatFacts: [CatFact] = [
+    {
+      _id: '12345',
+      text: 'Meow',
+      type: 'cat',
+      user: {
+        _id: '123',
+        name: {
+          first: 'John',
+          last: 'Doe',
+        },
+      },
+      upvotes: 1,
+      userUpvoted: null,
+    },
+  ];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,12 +36,37 @@ describe('BodyComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BodyComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
   describe('#render', () => {
     it('should create body component', () => {
       expect(component).toBeTruthy();
+    });
+
+    it('should render given list of cat facts', () => {
+      const expectedText = mockCatFacts[0].text;
+      component.catFacts = mockCatFacts;
+      fixture.detectChanges();
+
+      const tableRow = debugElement.query(By.css('#cat-facts-table-body')).children[0];
+      const actualText = tableRow.children[0].nativeElement.innerHTML;
+
+      expect(actualText).toEqual(expectedText);
+    });
+
+    it('should render writer full name when rendered', () => {
+      const userFirstName = mockCatFacts[0].user.name.first;
+      const userLastName = mockCatFacts[0].user.name.last;
+      const expectedName = `${userFirstName} ${userLastName}`;
+      component.catFacts = mockCatFacts;
+      fixture.detectChanges();
+
+      const tableRow = debugElement.query(By.css('#cat-facts-table-body')).children[0];
+      const actualRenderedName = tableRow.children[2].nativeElement.innerHTML;
+
+      expect(actualRenderedName).toEqual(expectedName);
     });
   });
 });
--- src/app/core/body/body.component.ts
+++ src/app/core/body/body.component.ts
@@ -1,10 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {CatFact} from '../../model/cat_fact.model';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css'],
 })
 export class BodyComponent implements OnInit {
+  @Input()
+  catFacts: [CatFact];
+
   ngOnInit(): void {
   }
 
