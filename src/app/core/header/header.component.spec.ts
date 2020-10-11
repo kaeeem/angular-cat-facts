f93bc3cd3f64e7f05f4e12270d97c24c637fa675
Kevin Mulia <kevin.mulia@btpn.com>
2020-10-12 00:24:17 +0700

    fix header component test, add more test case

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,7 +1,8 @@
 import {async, TestBed} from '@angular/core/testing';
 import {HeaderComponent} from './header.component';
+import {By} from '@angular/platform-browser';
 
-describe('AppComponent', () => {
+describe('HeaderComponent', () => {
   let fixture;
   let debugElement;
   let component;
@@ -18,11 +19,22 @@ describe('AppComponent', () => {
     fixture = TestBed.createComponent(HeaderComponent);
     debugElement = fixture.debugElement;
     component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   describe('#render', () => {
-    it('should create the app', () => {
+    it('should create app header', () => {
       expect(component).toBeTruthy();
+    });
+
+    it('should create h1 with given title', () => {
+      const expectedTitle = 'Cat Facts';
+
+      component.title = expectedTitle;
+      fixture.detectChanges();
+      const actualTitle = debugElement.query(By.css('h1')).nativeElement.innerHTML;
+
+      expect(actualTitle).toEqual(expectedTitle);
     });
   });
 });
